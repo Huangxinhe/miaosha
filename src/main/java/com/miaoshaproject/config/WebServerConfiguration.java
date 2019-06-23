9f23d9ce4b4f44543f5cf922f31a37e54decab59
huangxinhe <946840567@qq.com>
2019-06-23 23:01:06 +0800

    定制化tomcat开发

---
+++ src/main/java/com/miaoshaproject/config/WebServerConfiguration.java
@@ -0,0 +1,35 @@
+package com.miaoshaproject.config;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+/**
+ * @program miaosha
+ * @description: webserver
+ * @author: huangxinhe
+ * @create: 2019/06/23 22:46
+ */
+//当Spring容器内没有TomcatEmbeddedServletContainerFactory这个bean的时候，会把此bean加载到Spring中
+@Component
+public class WebServerConfiguration implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+    @Override
+    public void customize(ConfigurableWebServerFactory configurableWebServerFactory) {
+        //  使用对应工厂类提供给我们的接口定制化我们的tomcat connector
+        ((TomcatServletWebServerFactory)configurableWebServerFactory).addConnectorCustomizers(new TomcatConnectorCustomizer() {
+            @Override
+            public void customize(Connector connector) {
+              Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+
+              //定制化keepaliveTimeout,设置30秒内没有请求则服务器自动断开keepalive连接
+                protocol.setKeepAliveTimeout(30000);
+                //当客户端发送超过10000个请求则自动断开keepalive链接
+                protocol.setMaxKeepAliveRequests(10000);
+            }
+        });
+    }
+}
